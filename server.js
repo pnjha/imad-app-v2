4b8c8d757dce64ed9bec1b993e59ec6c5c31f8e2
pnjha <prakashecdrid@gmail.com>
2017-02-13 20:40:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 var config = {
   user:'pnjha',
   database:'pnjha',
-  host:'db.imad.hasura.app.io',
+  host:'db.imad.hasura-app.io',
   port: '5432',
   password:process.env.DB_PASSWORD
 };
